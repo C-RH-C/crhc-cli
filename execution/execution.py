8080f3427d45b48c4db68435994fd86b78246431
Waldirio M Pinheiro <waldirio@gmail.com>
2021-08-25 01:58:07 -0700

    fix 9 issue

--- execution/execution.py
+++ execution/execution.py
@@ -72,7 +72,7 @@ def swatch_list():
     url = "https://console.redhat.com/api/rhsm-subscriptions/v1/hosts/products/RHEL?limit=100&offset=0&sort=display_name"
     response = requests.get(url, auth=(USER, PASSWORD))
     check_authentication(response)
-    print(json.dumps(response.json(), indent=4))
+    return response.json()
 
 
 def swatch_list_all():
@@ -84,10 +84,57 @@ def swatch_list_all():
     check_authentication(response)
     num_of_pages = round(response.json()['meta']['count'] / 100 + 1)
 
+    dic_full_list = {'data':''}
+    full_list = []
+
     count = 0
     for page in range(0, num_of_pages):
         url = "https://console.redhat.com/api/rhsm-subscriptions/v1/hosts/products/RHEL?limit=100&offset=" + str(count) + "&sort=display_name"
         count = count + 100
 
         response = requests.get(url, auth=(USER, PASSWORD))
-        print(json.dumps(response.json(), indent=4))
+
+        for entry in response.json()['data']:
+            full_list.append(entry)
+
+    dic_full_list['data'] = full_list
+    return dic_full_list
+
+
+def swatch_socket_summary():
+    """
+    This def will present all the usage information in socket and
+    summary format from Subscription Watch
+    """
+
+    item_list = swatch_list_all()
+
+    baremetal_count = 0
+    hypervisor_count = 0
+    vm_with_no_host_guest_mapping = 0
+    cloud_count = 0
+    total_socket_count = 0
+
+    for server in item_list['data']:
+        # Baremetal server
+        if server['hardware_type'] == 'PHYSICAL' and server['is_hypervisor'] == False:
+            baremetal_count = baremetal_count + server['sockets']
+        # Hypervisor server
+        if server['hardware_type'] == 'PHYSICAL' and server['is_hypervisor'] == True:
+            hypervisor_count = hypervisor_count + server['sockets']
+        # VM with no host guest mapping
+        if server['hardware_type'] == 'VIRTUALIZED' and server['is_hypervisor'] == False and server['is_unmapped_guest'] == True:
+            vm_with_no_host_guest_mapping = vm_with_no_host_guest_mapping + server['sockets']
+        # Cloud server
+        if server['hardware_type'] == 'CLOUD':
+            cloud_count = cloud_count + server['sockets']
+    
+        total_socket_count = total_socket_count + server['sockets']
+
+
+    print("Public Cloud ........: {}".format(cloud_count))
+    print("Virtualized RHEL ....: {}".format(vm_with_no_host_guest_mapping))
+    print("Physical RHEL .......: {}".format(baremetal_count))
+    print("Hypervisors .........: {}".format(hypervisor_count))
+    print("----------------------")
+    print("Total # of Sockets ..: {}".format(total_socket_count))
