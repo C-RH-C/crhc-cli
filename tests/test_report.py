2457a0505fc9e6e5201e2444834f78aadd07b066
Waldirio M Pinheiro <waldirio@gmail.com>
2021-10-05 18:19:19 -0700

    after pylint and flake8

--- tests/test_report.py
+++ tests/test_report.py
@@ -1,17 +1,26 @@
-from report import report
+"""
+Test to check the Inventory Report
+"""
 import json
 import csv
+from report import report
+
 
 INPUT_JSON = "tests/data/inventory.json"
 OUTPUT_CSV = "/tmp/inventory_report.csv"
 
+
 def calling_csv_report_inventory():
+    """
+    Function responsible to read the template data and push against the
+    function
+    """
 
     # Reading the Input data that will be used in this test
     with open(INPUT_JSON, "r") as file_obj:
         aux = json.load(file_obj)
         report.csv_report_inventory(aux)
-        pass
+
 
 def test_csv_report_inventory():
     """
@@ -23,15 +32,13 @@ def test_csv_report_inventory():
     calling_csv_report_inventory()
 
     #  Counting 41 fields from Inventory Report
-    with open (OUTPUT_CSV, "r") as file_obj:
+    with open(OUTPUT_CSV, "r") as file_obj:
         aux = csv.reader(file_obj)
-        for b in aux:
-            print(b)
-            assert len(b) == 41
-            pass
+        for line in aux:
+            print(line)
+            assert len(line) == 41
 
     # Counting 3 rows, header + 2 lines from the input data
-    with open (OUTPUT_CSV, "r") as file_obj:
+    with open(OUTPUT_CSV, "r") as file_obj:
         aux = file_obj.readlines()
         assert len(aux) == 3
-        pass
