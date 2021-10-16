ee8a4f57c974cb99df3c8e6c0c2c376d17d95e8d
Waldirio M Pinheiro <waldirio@gmail.com>
2021-10-16 03:35:58 -0700

    [VERSION] version updated

--- crhc.py
+++ crhc.py
@@ -6,7 +6,7 @@ console.redhat.com (Inventory and Subscription Watch)
 
 from parse import parse
 
-CURRENT_VERSION = "1.6.6"
+CURRENT_VERSION = "1.7.7"
 
 if __name__ == "__main__":
     parse.main_menu()
