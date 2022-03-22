a748d94cfc1bae65a4a0994d5d9982716dcbd943
Waldirio M Pinheiro <waldirio@gmail.com>
2022-03-22 01:22:14 -0700

    spaces removed before the |tee

--- tests/end-to-end_crhc-cli_check.sh
+++ tests/end-to-end_crhc-cli_check.sh
@@ -23,17 +23,17 @@ LOG="/tmp/crhc-system-test_$DATE.log"
 
 check_packages()
 {
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - Checking for python3 package"													| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - Checking for python3 package" | tee -a $LOG
   check_python3=$(rpm -qa | grep ^python3 | wc -l)
   if [ $check_python3 == 0 ]; then
-    echo "exiting ... please, install python3 package"																				| tee -a $LOG
+    echo "exiting ... please, install python3 package" | tee -a $LOG
     exit
   fi
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - Checking for git package"															| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - Checking for git package" | tee -a $LOG
   check_python3=$(rpm -qa | grep ^git | wc -l)
   if [ $check_python3 == 0 ]; then
-    echo "exiting ... please, install git package"																						| tee -a $LOG
+    echo "exiting ... please, install git package" | tee -a $LOG
     exit
   fi
 }
@@ -47,15 +47,14 @@ virtualenv()
   fi
 
   if [ -d /tmp/crhc-cli ]; then
-    echo "## $(date +%m-%d-%Y_%H:%M:%S) - Removing current /tmp/crhc-cli dir ..."							| tee -a $LOG
-    rm -rf /tmp/crhc-cli																																			| tee -a $LOG
+    echo "## $(date +%m-%d-%Y_%H:%M:%S) - Removing current /tmp/crhc-cli dir ..." | tee -a $LOG
+    rm -rf /tmp/crhc-cli | tee -a $LOG
   fi
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - Cloning the crhc-cli repository: $url"								| tee -a $LOG
-  #url="https://github.com/C-RH-C/crhc-cli.git"
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - Cloning the crhc-cli repository: $url" | tee -a $LOG
   cd /tmp
   if [ "$1" <> " " ]; then
-    echo "## $(date +%m-%d-%Y_%H:%M:%S) - Cloning the crhc-cli repository from branch '$1'"		| tee -a $LOG
+    echo "## $(date +%m-%d-%Y_%H:%M:%S) - Cloning the crhc-cli repository from branch '$1'"	| tee -a $LOG
     git clone -b $1 $url
     if [ $? -ne 0 ]; then
       echo "something is wrong, probably the branch '$1' doesn't exist"
@@ -63,157 +62,157 @@ virtualenv()
       exit
     fi
   else
-    echo "## $(date +%m-%d-%Y_%H:%M:%S) - Cloning the crhc-cli repository from origin"				| tee -a $LOG
-    git clone $url																																						| tee -a $LOG
+    echo "## $(date +%m-%d-%Y_%H:%M:%S) - Cloning the crhc-cli repository from origin" | tee -a $LOG
+    git clone $url | tee -a $LOG
   fi
   cd crhc-cli
 
 
   if [ -d ~/.venv/crhc-cli ]; then
-    echo "## $(date +%m-%d-%Y_%H:%M:%S) - Removing current venv dir ..."											| tee -a $LOG
-    rm -rf ~/.venv/crhc-cli																																		| tee -a $LOG
+    echo "## $(date +%m-%d-%Y_%H:%M:%S) - Removing current venv dir ..." | tee -a $LOG
+    rm -rf ~/.venv/crhc-cli | tee -a $LOG
   fi
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - Creating the virtual env"															| tee -a $LOG
-  python3 -m venv ~/.venv/crhc-cli																														| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - Creating the virtual env" | tee -a $LOG
+  python3 -m venv ~/.venv/crhc-cli | tee -a $LOG
 
   if [ -d ~/.venv/crhc-cli ]; then
-    echo "## $(date +%m-%d-%Y_%H:%M:%S) - Virtual environment created successfully"						| tee -a $LOG
+    echo "## $(date +%m-%d-%Y_%H:%M:%S) - Virtual environment created successfully" | tee -a $LOG
   fi
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - Loading the Virtual Environment"											| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - Loading the Virtual Environment" | tee -a $LOG
   source ~/.venv/crhc-cli/bin/activate
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - Installing the requirements"													| tee -a $LOG
-  ~/.venv/crhc-cli/bin/pip install --upgrade pip																							| tee -a $LOG
-  ~/.venv/crhc-cli/bin/pip install -r requirements.txt																				| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - Installing the requirements" | tee -a $LOG
+  ~/.venv/crhc-cli/bin/pip install --upgrade pip | tee -a $LOG
+  ~/.venv/crhc-cli/bin/pip install -r requirements.txt | tee -a $LOG
 }
 
 
 inventory_def()
 {
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - Inventory Pipeline"										| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - Inventory Pipeline" | tee -a $LOG
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - inventory menu"												| tee -a $LOG
-  $CRHC inventory																															| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - inventory menu" | tee -a $LOG
+  $CRHC inventory | tee -a $LOG
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - inventory list"												| tee -a $LOG
-  $CRHC inventory list 																												| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - inventory list" | tee -a $LOG
+  $CRHC inventory list | tee -a $LOG
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - inventory list_all"										| tee -a $LOG
-  $CRHC inventory list_all 																										| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - inventory list_all" | tee -a $LOG
+  $CRHC inventory list_all | tee -a $LOG
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - inventory display_name"								| tee -a $LOG
-  $CRHC inventory display_name	  																						| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - inventory display_name" | tee -a $LOG
+  $CRHC inventory display_name | tee -a $LOG
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - inventory list --csv"									| tee -a $LOG
-  $CRHC inventory list --csv																									| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - inventory list --csv" | tee -a $LOG
+  $CRHC inventory list --csv | tee -a $LOG
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - inventory list_all --csv"							| tee -a $LOG
-  $CRHC inventory list_all --csv																							| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - inventory list_all --csv" | tee -a $LOG
+  $CRHC inventory list_all --csv | tee -a $LOG
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - inventory display_name esxi1"					| tee -a $LOG
-  $CRHC inventory display_name esxi1																					| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - inventory display_name esxi1" | tee -a $LOG
+  $CRHC inventory display_name esxi1 | tee -a $LOG
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - inventory display_name esxi1 --csv"		| tee -a $LOG
-  $CRHC inventory display_name esxi1 --csv																		| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - inventory display_name esxi1 --csv" | tee -a $LOG
+  $CRHC inventory display_name esxi1 --csv | tee -a $LOG
 }
 
 swatch_def()
 {
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - Swatch Pipeline"											| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - Swatch Pipeline" | tee -a $LOG
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - swatch menu"													| tee -a $LOG
-  $CRHC swatch																																| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - swatch menu" | tee -a $LOG
+  $CRHC swatch | tee -a $LOG
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - swatch list"													| tee -a $LOG
-  $CRHC swatch list 																													| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - swatch list" | tee -a $LOG
+  $CRHC swatch list | tee -a $LOG
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - swatch list_all"											| tee -a $LOG
-  $CRHC swatch list_all																												| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - swatch list_all" | tee -a $LOG
+  $CRHC swatch list_all | tee -a $LOG
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - swatch list --csv"										| tee -a $LOG
-  $CRHC swatch list --csv																											| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - swatch list --csv" | tee -a $LOG
+  $CRHC swatch list --csv | tee -a $LOG
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - swatch list_all --csv"								| tee -a $LOG
-  $CRHC swatch list_all --csv																									| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - swatch list_all --csv" | tee -a $LOG
+  $CRHC swatch list_all --csv | tee -a $LOG
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - swatch socket_summary"								| tee -a $LOG
-  $CRHC swatch socket_summary																									| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - swatch socket_summary" | tee -a $LOG
+  $CRHC swatch socket_summary | tee -a $LOG
 }
 
 patch_def()
 {
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - Patch Pipeline"	       								| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - Patch Pipeline" | tee -a $LOG
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - patch"				         								| tee -a $LOG
-  $CRHC patch 																																| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - patch" | tee -a $LOG
+  $CRHC patch | tee -a $LOG
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - patch systems"         								| tee -a $LOG
-  $CRHC patch systems																													| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - patch systems" | tee -a $LOG
+  $CRHC patch systems | tee -a $LOG
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - patch systems --csv"   								| tee -a $LOG
-  $CRHC patch systems	--csv																										| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - patch systems --csv" | tee -a $LOG
+  $CRHC patch systems	--csv | tee -a $LOG
 }
 
 vulnerability_def()
 {
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - Vulnerability Pipeline"								| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - Vulnerability Pipeline" | tee -a $LOG
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - vulnerability"	       								| tee -a $LOG
-  $CRHC vulnerability																													| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - vulnerability" | tee -a $LOG
+  $CRHC vulnerability | tee -a $LOG
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - vulnerability systems"  							| tee -a $LOG
-  $CRHC vulnerability systems																									| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - vulnerability systems" | tee -a $LOG
+  $CRHC vulnerability systems | tee -a $LOG
 	
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - vulnerability systems --csv"					| tee -a $LOG
-  $CRHC vulnerability systems	--csv																						| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - vulnerability systems --csv" | tee -a $LOG
+  $CRHC vulnerability systems	--csv | tee -a $LOG
 }
 
 advisor_def()
 {
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - Advisor Pipeline"											| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - Advisor Pipeline" | tee -a $LOG
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - advisor"	 														| tee -a $LOG
-  $CRHC advisor 																															| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - advisor" | tee -a $LOG
+  $CRHC advisor | tee -a $LOG
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - advisor systems"  										| tee -a $LOG
-  $CRHC advisor systems 																											| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - advisor systems" | tee -a $LOG
+  $CRHC advisor systems | tee -a $LOG
 	
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - advisor systems --csv"								| tee -a $LOG
-  $CRHC advisor systems	--csv 														 										| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - advisor systems --csv" | tee -a $LOG
+  $CRHC advisor systems	--csv | tee -a $LOG
 }
 
 ts_def()
 {
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - Ts Pipeline"    											| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - Ts Pipeline" | tee -a $LOG
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - ts menu"															| tee -a $LOG
-  $CRHC ts																																		| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - ts menu" | tee -a $LOG
+  $CRHC ts | tee -a $LOG
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - ts dump"															| tee -a $LOG
-  $CRHC ts dump																																| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - ts dump" | tee -a $LOG
+  $CRHC ts dump | tee -a $LOG
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - ts match"															| tee -a $LOG
-  $CRHC ts match																															| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - ts match" | tee -a $LOG
+  $CRHC ts match | tee -a $LOG
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - ts match (files already in place)"		| tee -a $LOG
-  $CRHC ts match																															| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - ts match (files already in place)" | tee -a $LOG
+  $CRHC ts match | tee -a $LOG
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - ts clean"															| tee -a $LOG
-  $CRHC ts clean																															| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - ts clean" | tee -a $LOG
+  $CRHC ts clean | tee -a $LOG
 }
 
 
 endpoint_def()
 {
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - Endpoint Pipeline"										| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - Endpoint Pipeline" | tee -a $LOG
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - endpoint list"												| tee -a $LOG
-  $CRHC endpoint list																													| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - endpoint list" | tee -a $LOG
+  $CRHC endpoint list | tee -a $LOG
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - checking all the endpoint"						| tee -a $LOG
-  for b in $($CRHC endpoint list | grep \" | cut -d\" -f2 | sed '1d'); do echo - $b; $CRHC get $b; echo; done 		| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - checking all the endpoint" | tee -a $LOG
+  for b in $($CRHC endpoint list | grep \" | cut -d\" -f2 | sed '1d'); do echo - $b; $CRHC get $b; echo; done | tee -a $LOG
 
   #$CRHC get <API ENDPOINT>
   #$CRHC get <API ENDPOINT>
@@ -223,63 +222,63 @@ endpoint_def()
 
 binary_def()
 {
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - Bynary Pipeline"              				| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - Bynary Pipeline" | tee -a $LOG
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - generating the bynary version"				| tee -a $LOG
-  ~/.venv/crhc-cli/bin/pyinstaller --onefile $CRHC 														| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - generating the bynary version" | tee -a $LOG
+  ~/.venv/crhc-cli/bin/pyinstaller --onefile $CRHC | tee -a $LOG
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - ldd binary file"											| tee -a $LOG
-  ldd /tmp/crhc-cli/dist/crhc											 														| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - ldd binary file" | tee -a $LOG
+  ldd /tmp/crhc-cli/dist/crhc | tee -a $LOG
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - executing the binary file"						| tee -a $LOG
-  /tmp/crhc-cli/dist/crhc											 																| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - executing the binary file" | tee -a $LOG
+  /tmp/crhc-cli/dist/crhc | tee -a $LOG
 }
 
 general_def()
 {
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - General Pipeline"											| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - General Pipeline" | tee -a $LOG
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - token"																| tee -a $LOG
-  $CRHC token																																	| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - token" | tee -a $LOG
+  $CRHC token | tee -a $LOG
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - whoami"																| tee -a $LOG
-  $CRHC whoami																																| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - whoami" | tee -a $LOG
+  $CRHC whoami | tee -a $LOG
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - '--version'"													| tee -a $LOG
-  $CRHC --version																															| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - '--version'" | tee -a $LOG
+  $CRHC --version | tee -a $LOG
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - '-v'"																	| tee -a $LOG
-  $CRHC -v																																		| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - '-v'" | tee -a $LOG
+  $CRHC -v | tee -a $LOG
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - '--help'"															| tee -a $LOG
-  $CRHC --help																																| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - '--help'" | tee -a $LOG
+  $CRHC --help | tee -a $LOG
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - '-h'"																	| tee -a $LOG
-  $CRHC -h																																		| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - '-h'" | tee -a $LOG
+  $CRHC -h | tee -a $LOG
 }
 
 logout_def()
 {
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - Logout Pipeline"												| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - Logout Pipeline" | tee -a $LOG
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - Logout"																	| tee -a $LOG
-  $CRHC logout																																	| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - Logout" | tee -a $LOG
+  $CRHC logout | tee -a $LOG
 }
 
 login_def()
 {
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - Login Pipeline"	   											| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - Login Pipeline" | tee -a $LOG
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - login with token"												| tee -a $LOG
-  $CRHC login --token $TOKEN																										| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - login with token" | tee -a $LOG
+  $CRHC login --token $TOKEN | tee -a $LOG
 }
 
 pytest_def()
 {
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - Pytest Pipeline"												| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - Pytest Pipeline" | tee -a $LOG
 
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - pytest -v"															| tee -a $LOG
-  pytest -v																																			| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - pytest -v" | tee -a $LOG
+  pytest -v	| tee -a $LOG
 }
 
 
@@ -291,7 +290,7 @@ pytest_def()
 #
 
 
-echo "## $(date +%m-%d-%Y_%H:%M:%S) - STARTING"																| tee -a $LOG
+echo "## $(date +%m-%d-%Y_%H:%M:%S) - STARTING" | tee -a $LOG
 if [ ! -f ~/.token ]; then
   echo "Please, create the file '~/.token' with the content as below. Update the token information properly, based on your own token"
   echo "TOKEN=\"eyJhbGciOiJIUzI1NiIsInR5cCIg...\""
@@ -343,14 +342,14 @@ fi
 
 # Passing the branch and also the pipeline
 if [ "$1" == "-b" ] && [ "$3" == "-p" ] && [ "$5" == "-g" ]; then
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - Passing the branch: $2,the pipeline: $4 and the repo: $6"		| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - Passing the branch: $2,the pipeline: $4 and the repo: $6" | tee -a $LOG
   branch=$2
   pipeline=$4
   gitrepo=$6
 fi
 
 if [ "$1" == "-p" ]; then
-  echo "## $(date +%m-%d-%Y_%H:%M:%S) - Using the origin branch and the pipeline: $2"								| tee -a $LOG
+  echo "## $(date +%m-%d-%Y_%H:%M:%S) - Using the origin branch and the pipeline: $2" | tee -a $LOG
   pipeline=$2
 fi
 
@@ -453,4 +452,4 @@ case $pipeline in
      ;;
 esac
 
-echo "## $(date +%m-%d-%Y_%H:%M:%S) - ENDING"																	| tee -a $LOG
+echo "## $(date +%m-%d-%Y_%H:%M:%S) - ENDING" | tee -a $LOG
